26d5dce04f93f4430dc64e73c6aa4cfc21d8eb18
Avinash Doniparthi <avichitti7@gmail.com>
2026-08-22 10:04:52 +0530

    Added 5th Problem

---
+++ Armstrong.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int length = 0;
+
+        // Find length
+        for (int i = 0; temp > 0; i++) {
+            length++;
+            temp = temp / 10;
+        }
+        temp = num;
+        int sum = 0;
+
+        for (int i = 0; i < length; i++) {
+            int digit = temp % 10;
+            sum = sum + (int)Math.pow(digit, length);
+            temp = temp / 10;
+        }
+        if (sum == num)
+            System.out.println("Armstrong number");
+        else
+            System.out.println("Not an Armstrong number");
+    }
+}
